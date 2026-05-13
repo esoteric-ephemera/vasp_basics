bc157b5c1aae198aabdc0d465a58c1d66dc3aa55
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2026-05-13 15:01:40 -0700

    typo

--- 2-mp-workflow.py
+++ 2-mp-workflow.py
@@ -232,6 +232,7 @@ def run_mp_workflow_locally(
 
     cwd = Path.cwd()
     with chdir_ctx(working_dir):
+        return
         response = run_locally(
             mp_workflow(structure,incar_updates=incar_updates),
             store=get_job_store(base_path=working_dir),
